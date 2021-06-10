2a91d41fba24822eecd6b54b2d1230280f0b8d37
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-06-11 02:35:58 +0300

    set rules for code formating & format TS code

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ class ProjectInput {
   element: HTMLFormElement;
 
   constructor() {
-    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
     this.attach();
   }
 
-  private attach = () => this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  private attach = () => this.hostElement.insertAdjacentElement('afterbegin', this.element);
 }
 
 const prjInput = new ProjectInput();
